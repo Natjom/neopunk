aec812f944c2dba3401b89394c0579e3301362db
Natjom <n4t10m@gmail.com>
2025-09-30 03:58:25 +0200

    0.0.3

--- src/main/java/natjom/neopunk/ui/CustomInventoryOpener.java
+++ src/main/java/natjom/neopunk/ui/CustomInventoryOpener.java
@@ -2,15 +2,12 @@ package natjom.neopunk.ui;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.chat.Component;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.SimpleMenuProvider;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraft.client.gui.screens.inventory.InventoryScreen;
-import net.minecraftforge.network.NetworkHooks;
 
 @Mod.EventBusSubscriber(modid = "neopunk", value = Dist.CLIENT)
 public class CustomInventoryOpener {
@@ -22,12 +19,13 @@ public class CustomInventoryOpener {
             if (player != null && !player.isCreative()) {
                 event.setCanceled(true);
 
-                if (player instanceof ServerPlayer serverPlayer) {
-                    NetworkHooks.openScreen(serverPlayer, new SimpleMenuProvider(
-                            (id, inv, p) -> new CustomInventoryMenu(id, inv),
-                            Component.literal("Inventaire Custom")
-                    ));
-                }
+                Minecraft.getInstance().setScreen(
+                        new CustomInventoryScreen(
+                                new CustomInventoryMenu(0, player.getInventory()),
+                                player.getInventory(),
+                                Component.literal("Inventaire Custom")
+                        )
+                );
             }
         }
     }
