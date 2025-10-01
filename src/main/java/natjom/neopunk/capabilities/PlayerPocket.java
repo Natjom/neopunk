22dd19891bac74d58b64a2327f6e6ff820f4cd9c
Natjom <n4t10m@gmail.com>
2025-10-01 03:55:03 +0200

    0.0.7

--- src/main/java/natjom/neopunk/capabilities/PlayerPocket.java
+++ src/main/java/natjom/neopunk/capabilities/PlayerPocket.java
@@ -2,27 +2,59 @@ package natjom.neopunk.capabilities;
 
 import net.minecraft.core.Direction;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraftforge.common.capabilities.Capability;
-import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.common.capabilities.CapabilityToken;
-import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.common.capabilities.*;
 import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.items.ItemStackHandler;
-import net.minecraftforge.items.IItemHandlerModifiable;
 
+@Mod.EventBusSubscriber(modid = "neopunk")
 public class PlayerPocket {
-    public static final Capability<IItemHandlerModifiable> POCKET =
+    public static final Capability<ItemStackHandler> POCKET =
             CapabilityManager.get(new CapabilityToken<>() {});
 
-    public static class Provider implements ICapabilityProvider {
+    public static class Provider implements ICapabilitySerializable<CompoundTag> {
         private final ItemStackHandler handler = new ItemStackHandler(12);
+        private final LazyOptional<ItemStackHandler> optional = LazyOptional.of(() -> handler);
 
         @Override
         public <T> LazyOptional<T> getCapability(Capability<T> cap, Direction side) {
-            return cap == POCKET ? LazyOptional.of(() -> handler).cast() : LazyOptional.empty();
+            return cap == POCKET ? optional.cast() : LazyOptional.empty();
         }
 
-        public CompoundTag serializeNBT() { return handler.serializeNBT(); }
-        public void deserializeNBT(CompoundTag nbt) { handler.deserializeNBT(nbt); }
+        @Override
+        public CompoundTag serializeNBT() {
+            return handler.serializeNBT();
+        }
+
+        @Override
+        public void deserializeNBT(CompoundTag nbt) {
+            handler.deserializeNBT(nbt);
+        }
+    }
+
+    @SubscribeEvent
+    public static void onAttachCapabilities(AttachCapabilitiesEvent<Object> event) {
+        if (event.getObject() instanceof Player) {
+            event.addCapability(new ResourceLocation("neopunk", "pocket"), new Provider());
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerClone(PlayerEvent.Clone event) {
+        if (!event.isWasDeath()) return;
+        event.getOriginal().reviveCaps();
+
+        event.getOriginal().getCapability(POCKET).ifPresent(oldInv -> {
+            event.getEntity().getCapability(POCKET).ifPresent(newInv -> {
+                newInv.deserializeNBT(oldInv.serializeNBT());
+            });
+        });
+
+        event.getOriginal().invalidateCaps();
     }
 }
